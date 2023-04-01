bf3530df52a4124e13f3d75496a3b508a5954fea
Diego Valencia <ve18007@ues.edu.sv>
2023-03-31 19:03:34 -0600

    Add input for the countries of the world

---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <a-auto-complete
+      v-model:value="value"
+      :options="options"
+      class="input-principal"
+      placeholder="input here"
+      @search="onSearch"
+    />
+  </div>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+  const value = ref("");
+  const options = ref([]);
+
+  const mockVal = (str, repeat = 1) => {
+  return {
+    value: str.repeat(repeat),
+  };
+};
+
+  const onSearch = (searchText) => {
+    options.value = !searchText
+      ? []
+      : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)];
+  };
+
+
+</script>
+
+<style scoped>
+  .input-principal {
+    width: 80%;
+    height: 40px;
+    margin-top: 40px;
+  }
+
+  .container {
+    display: flex;
+    justify-content: center;
+    height: 78vh;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
+<template>
+  <a-layout>
+    <a-layout-header class="bg-all">
+      <router-link to="/">
+        <img
+          :width="110"
+          src="https://santatecla.gob.sv/wp-content/uploads/2022/06/LOGOAzUL.png"
+        />
+      </router-link>
+    </a-layout-header>
+    <a-layout-content class="bg-all">
+      <Home />
+    </a-layout-content>
+    <a-layout-footer class="bg-all">
+      Prueba tecnica para alcaldia de Santa Tecla :)
+    </a-layout-footer>
+  </a-layout>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  import Home from "../components/Home.vue";
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+  .bg-all {
+    background-color: #fff;
+  }
+</style>
